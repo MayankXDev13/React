5018676e692705cad78d89f8a7c31fb32534781c
Mayank <vijaymaynk2003@gmail.com>
2025-04-24 15:53:22 +0530

    Initialize 02counter project with React and Vite setup
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application layout and components.
    - Implemented App.jsx with a counter functionality using React hooks.
    - Added React logo SVG to assets directory.
    - Created index.css for global styles and theming.
    - Set up main.jsx as the entry point for the React application.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+  const removeValue = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+    setCounter((prevCounter) => prevCounter - 1);
+    setCounter((prevCounter) => prevCounter - 1);
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
+  return (
+    <>
+      <h1> React course with hitesh {counter}</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
